8c6aa75b6b3cec9aa2f60668fa2ed2c36dd186b0
Shawn <shawn17@live.fr>
2021-10-26 16:42:15 +0200

    adding swal effect, confirmation to cart adding

--- src/screens/BuyProduct/BuyProduct.jsx
+++ src/screens/BuyProduct/BuyProduct.jsx
@@ -3,6 +3,8 @@ import Navbar from "../../components/Navbar/Navbar";
 
 import { BsCartPlus } from 'react-icons/bs'
 
+import Swal from "sweetalert2";
+
 import './BuyProduct.css' 
 
 import BuyImg1 from '../../assets/fork1-wbg.png'
@@ -19,6 +21,35 @@ const BuyProduct = () => {
   const [currentPhoto, setCurrentPhoto] = useState(BuyImg1)
   const [currentQuantity, setCurrentQuantity] = useState(1)
   const [currentColor, setCurrentColor] = useState()
+
+  const handleAddingToCart = () => {
+    if(!currentColor) {
+      Swal.fire({
+        title: 'Vous devez sélectionner une couleur',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+    })
+  } else {
+    const incomingPurchase = {
+      'quantity': currentQuantity,
+      'color': currentColor,
+      'price': Number.parseFloat(currentQuantity * 49.99).toFixed(2)
+    }
+    Swal.fire({
+      title: 'Bien ajouté',
+      html: 'Votre produit a bien été ajouté à votre panier',
+      icon: 'success',
+      showCloseButton: true,
+      showCancelButton: true,
+      confirmButtonColor: 'orange',
+      cancelButtonColor: 'silver',
+      cancelButtonText: 'Retour au produit',
+      confirmButtonText: 'Voir mon panier'
+    })
+    console.log(incomingPurchase)
+  }
+  
+  }
 
   console.log(currentColor)
 
@@ -73,7 +104,7 @@ const BuyProduct = () => {
           </div>
           <div className='buy-product-price'>
             <h4><span className='previous-price'>{Number.parseFloat(currentQuantity * 69.99).toFixed(2)+ '€'}</span>{Number.parseFloat(currentQuantity * 49.99).toFixed(2) + '€'}</h4>
-            <button><BsCartPlus />&nbsp;&nbsp; Ajouter au panier</button>
+            <button onClick={handleAddingToCart}><BsCartPlus />&nbsp;&nbsp; Ajouter au panier</button>
           </div>
         </div>
         
